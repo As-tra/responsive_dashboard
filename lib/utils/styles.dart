d52f816bc1eefdc5c44c5ecc1a21a1c28a6a2d0e
Assil <astradhdbjd@gmail.com>
2024-07-15 16:13:09 +0100

    add styles for the project

---
+++ lib/utils/styles.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/constants.dart';
+
+abstract class Styles {
+  static const styleRegular16 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 16,
+    color: Color(0xff064061),
+  );
+  static const styleMeduim16 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff064061),
+  );
+  static const styleSemiBold16 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064061),
+  );
+  static const styleBold16 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Color(0xff4EB7F2),
+  );
+  static const styleSemiBold20 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064061),
+  );
+  static const styleMeduim20 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Color(0xFFFFFFFF),
+  );
+  static const styleRegular12 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 12,
+    color: Color(0xffaaaaaa),
+  );
+  static const styleSemiBold24 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff4EB7F2),
+  );
+  static const styleRegular14 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 14,
+    color: Color(0xffaaaaaa),
+  );
+  static const styleSemiBold18 = TextStyle(
+    fontFamily: kFontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff4EB7F2),
+  );
+}
