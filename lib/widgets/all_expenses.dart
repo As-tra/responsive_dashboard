c15cac97ebacd09796e72f17fa6ef8c3f1e477d2
Assil <astradhdbjd@gmail.com>
2024-07-17 11:43:02 +0100

    create the all expenses header

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard/utils/styles.dart';
+import 'package:responsive_dashboard/widgets/all_expenses_header.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
@@ -21,18 +21,3 @@ class AllExpenses extends StatelessWidget {
   }
 }
 
-class AllExpensesHeader extends StatelessWidget {
-  const AllExpensesHeader({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Text(
-          'All Expenses',
-          style: Styles.styleSemiBold20,
-        ),
-      ],
-    );
-  }
-}
